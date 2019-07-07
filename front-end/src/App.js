4055e3417ce0a6f49dbbbb69d4743ed74ed17833
Han Xu <hanxu27@gmail.com>
2019-07-07 18:17:15 -0400

    navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import { BrowerRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './containers/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { NavigationBar } from './containers/NavigationBar';
 import Main from './containers/Main';
 import Parks from './containers/Parks'
 import Visit from './containers/Visit';
 import UserPage from './containers/UserPage'
-import Layout from './containers/Layout';
+import { NoMatch } from './components/NoMatch'
+import { Layout } from './containers/Layout';
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar>
-        <Layout>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Main} />
-              <Route path="/parks" component={Parks} />
-              <Route path="/visit" component={Visit} />
-              <Route path="/login" component={UserPage} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Router>
-        </Layout>
+      <NavigationBar />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path="/parks" component={Parks} />
+            <Route path="/visits" component={Visit} />
+            <Route path="/login" component={UserPage} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
     </React.Fragment>
-      );
-    }
-    
-    export default App;
+  );
+}
+
+export default App;
---
+++ front-end/src/containers/NavigationBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+import styled from 'styled-components'
+
+const Styles = styled.div`
+  .navbar{
+    background-color: #222;
+  }
+
+  .navbar-brand, .navbar-nav .nav-link{
+    color: #bbb;
+
+    &:hover {
+      color: white;
+    }
+  }
+`;
+
+export const NavigationBar = ()=> (
+  <Styles>
+    <Navbar expand="sm">
+    <Navbar.Brand href="/">Hotlanta</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" >
+        <Nav className='ml-auto'>
+          <Nav.Item><Nav.Link href="/">Main</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/parks">Parks</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/visits">Visits</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
+        </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+)
