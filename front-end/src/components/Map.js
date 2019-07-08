e153a48e622d742cf175ae434bcb2b1a4135921d
Han Xu <hanxu27@gmail.com>
2019-07-08 14:41:15 -0400

    very commit

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react'
-import ReactMap, { Marker } from 'react-map-gl';
+import React, { useState, useEffect } from 'react'
+import ReactMap, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/src/css/mapbox-gl.css'
+import { ParkCard } from './ParkCard'
 
 export const Map = (props) => {
   const [viewport, setViewport] = useState({
     latitude: 38.560954,
     longitude: -98.935240,
     zoom: 4,
-    width: '90vw',
-    height: '85vh'
+    width: '100vw',
+    height: '90vh'
   })
+  const [selected, setSelected] = useState(null)
+
+  const getLat = (park) => park.latLong.slice(4).split(", long:").map(num => parseFloat(num))[0]
+  const getLong = (park) => park.latLong.slice(4).split(", long:").map(num => parseFloat(num))[1]
+
+  useEffect(() => {
+    const listener = e => e.key === "Escape" && setSelected(null)
+    window.addEventListener("keydown", listener)
+    return () => window.removeEventListener("keydown", listener)
+  }, [])
 
   const parkMarker = park => {
-    const lat = park.latLong.slice(4).split(", long:").map(num => parseFloat(num))[0]
-    const long = park.latLong.slice(4).split(", long:").map(num => parseFloat(num))[1]
     return (
       <Marker
         key={park.id}
-        latitude={lat}
-        longitude={long}
+        latitude={getLat(park)}
+        longitude={getLong(park)}
       >
-        <button className="marker-btn" >
+        <button className="marker-btn" onClick={e => {
+          e.preventDefault()
+          setSelected(park)
+        }} >
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/1/1d/US-NationalParkService-Logo.svg"
             alt="Park Logo" />
@@ -38,6 +50,11 @@ export const Map = (props) => {
         onViewportChange={viewport => setViewport(viewport)}
       >
         {props.parks.map(parkMarker)}
+        {selected && (
+          < Popup latitude={getLat(selected)} longitude={getLong(selected)} onClose={() => setSelected(null)}>
+            <ParkCard park={selected} />
+          </Popup>
+        )}
       </ReactMap>
     </div >
   )
