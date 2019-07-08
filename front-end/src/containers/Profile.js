a6ec4e67616b62ab313f3b38bc121cd3188695cd
ascotttoney <ascotttoney@gmail.com>
2019-07-08 15:05:59 -0400

    Monday afternoon.

---
+++ front-end/src/containers/Profile.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap'
+
+export default class Profile extends Component {
+  render() {
+    return(
+      <Card style={{width: '70%'}}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Body>
+          <Card.Title>Hey, {this.props.user.firstName}!</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+        </Card.Body>
+
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>National Parks Visited:</ListGroupItem>
+          <ListGroupItem>National Parks Yet To Visit:</ListGroupItem>
+          <ListGroupItem>Some Other Stuff?</ListGroupItem>
+        </ListGroup>
+
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+
+        <Button variant="primary" type="submit">
+          Log Out (doesn't work)
+        </Button>
+      </Card>
+    )
+  }
+}
