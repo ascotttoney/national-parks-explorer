768eff27136f1a430a6d567c7f56e500df0b4b5e
ascotttoney <ascotttoney@gmail.com>
2021-06-21 11:04:08 -0400

    cleanup

--- front-end/src/components/NoMatch.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const NoMatch = () => {
-	return (
-		<div>
-			<h2>Sorry, I don't know what to do.</h2>
-		</div>
-	);
-};
--- front-end/src/containers/Layout.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Layout = (props) => {
-	return <React.Fragment>{props.children}</React.Fragment>;
-};
